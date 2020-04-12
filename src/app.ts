1f4d885f93aad9fc53fcd3e09cca587c23cf3ca1
pradeepanvi <pradeepkumar.anvi@gmail.com>
2020-04-12 14:12:08 +0530

    Creating & Using and Autobind

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
+// Autobind Decorator
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -14,16 +28,18 @@ class ProjectInput {
         this.element = importNode.firstElementChild as HTMLFormElement;
         this.element.id = 'user-input';
 
-        this.titleInputElement = document.querySelector('#title') as HTMLInputElement;
-        this.descriptionInputElement = document.querySelector('#description') as HTMLInputElement;
-        this.peopleInputElement = document.querySelector('#people') as HTMLInputElement;
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
 
         this.configure();
         this.attach();
     }
 
+    @Autobind
     private submitHandler(event: Event) {
         event.preventDefault();
+        console.log(this.titleInputElement.value);
     }
 
     private configure() {
